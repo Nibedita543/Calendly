628341be4a1cf81708331555928cddbd891f1de0
Nibedita Bal <nibeditabal2001@gmail.com>
2025-03-27 10:49:45 +0530

    test commit

--- src/Components/SignUp/SignUpRIght.jsx
+++ src/Components/SignUp/SignUpRIght.jsx
@@ -20,8 +20,14 @@ export default function SignUpRIght() {
   ];
 
   const imageurls = [
-    "https://images.ctfassets.net/k0lk9kiuza3o/bOnLOncE…30cfff0a87e8dce2bced46e/dropbox-customer-logo.svg",
-    "https://images.ctfassets.net/k0lk9kiuza3o/6aSAknGk…686e4a2b6e99348e1899fc/ancestry-customer-logo.svg"
+    "https://images.ctfassets.net/k0lk9kiuza3o/bOnLOncEyDdb8izczJggp/b98f4d8da30cfff0a87e8dce2bced46e/dropbox-customer-logo.svg",
+    "https://images.ctfassets.net/k0lk9kiuza3o/6aSAknGku3oEB7jL7xEjLs/7004d26e48686e4a2b6e99348e1899fc/ancestry-customer-logo.svg",
+    "https://images.ctfassets.net/k0lk9kiuza3o/4AUQ47IN6ZBtXtWXt65L3D/24dae32ff49baaf8feecf9471804420d/zendesk-customer-logo.svg",
+    "https://images.ctfassets.net/k0lk9kiuza3o/k5u1aoWgLdVtFfc4tsyg3/cae65a31428205e2b46b229963d3b41e/Gong-logo.svg",
+    "https://images.ctfassets.net/k0lk9kiuza3o/6PFPRWMRMxRXthyXYcDRiR/ee7cc3f3ca0ed78752db06ce662a95f8/compass-customer-logo.svg",
+    "https://images.ctfassets.net/k0lk9kiuza3o/Y595RQBDR1fW5blQZfd9l/b9f3e2557c598b1ea51bb4e55f507833/lyft-customer-logo.svg",
+    "https://images.ctfassets.net/k0lk9kiuza3o/42drnxHfXrNOGKnVE9iA3r/a7f1bea3f67ca614e359eb6de12d8ba1/loreal-customer-logo.svg",
+    "https://images.ctfassets.net/k0lk9kiuza3o/56cjEhKMIFlRPdVUSQqMYD/a33ef976df35d4e240246d5e08bcb610/crocs-customer-logo.svg",
   ];
 
   return (
