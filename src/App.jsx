4e27ebd59762e92e04d01b74883dd9c9f78f49bb
niharsasini <niharsasini@gmail.com>
2025-03-27 11:14:34 +0530

    Added Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import SignUp from "./Components/SignUp/SignUp";
 import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   return (
-    <Router>
+    <>
       <Navbar />
       <Routes>
         <Route path="/" element={<SignUp />} />
       </Routes>
-    </Router>
+    </>
   );
 }
 
