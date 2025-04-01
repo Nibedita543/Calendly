aa0402abf5f371d90772182747447c689a0577e4
Nibedita Bal <nibeditabal2001@gmail.com>
2025-04-01 16:14:40 +0530

    dashboard started

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,11 +1,57 @@
-import React from 'react'
+import { Box, Button, Drawer } from "@mui/material";
+import React from "react";
+import LeftCollapse from "../../assets/left-collapse-icon.svg"
+import { BiPlus } from "react-icons/bi";
 
-const Sidebar = () => {
+export default function Sidebar() {
   return (
-    <div>
-      
-    </div>
-  )
+    <Drawer
+      open={true}
+      variant="persistent"
+      sx={{
+        width: 300,
+        flexShrink: 0,
+        [`& .MuiDrawer-paper`]: {
+          width: 260,
+          boxSizing: "border-box",
+          padding: 2,
+        },
+      }}
+    >
+      <Box
+        display={"flex"}
+        alignItems={"center"}
+        justifyContent={"space-between"}
+      >
+        <Box sx={{ display: "flex", alignItems: "center", gap: 1, mb: 2 }}>
+          <img
+            src="https://assets.calendly.com/assets/frontend/media/calendly-33a0809afc4c21162dd7.svg"
+            style={{ width: "28px", height: "28px" }}
+          />
+          <img
+            src="https://assets.calendly.com/assets/frontend/media/calendly-wordmark-0da6c58d9a06b08c975f.svg"
+            style={{ marginTop: "6px" }}
+          />
+        </Box>
+        <img src={LeftCollapse} style={{ marginBottom: "10px" }} />
+      </Box>
+      <Button
+        variant="outlined"
+        sx={{
+          maxWidth: "100%",
+          mb: 2,
+          mt: 2,
+          borderRadius: "30px",
+          fontWeight: 400,
+          textTransform: "none",
+          height: "45px",
+          border: "1px solid blue",
+          "&:hover": { backgroundColor: "#E6F0FF" },
+        }}
+      >
+        <BiPlus size={"16px"} />
+        Create
+      </Button>
+    </Drawer>
+  );
 }
-
-export default Sidebar
