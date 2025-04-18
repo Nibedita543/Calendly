4dd15b18fdc941b5ec53dd3ccb9677a6f8ae4d65
Nibedita Bal <nibeditabal2001@gmail.com>
2025-04-18 18:23:25 +0530

    admin sidebar completed

--- src/Components/Sidebar/AdminSidebardata.jsx
+++ src/Components/Sidebar/AdminSidebardata.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import dashboard from '../../assets/dashboard.svg'
 import user from '../../assets/user.svg'
+import login from '../../assets/login.svg'
 import groups from '../../assets/groups.svg'
+import bill from '../../assets/bill.svg'
+import security from '../../assets/security.svg'
+import permission from '../../assets/permission.svg'
+import manageevents from '../../assets/manageevents.svg'
+import workflow from '../../assets/workflow.svg'
 import { NAVIGATION_ROUTES } from '../../constants/NavigationRoutes'
 
 export const AdminSidebardata = [
@@ -13,11 +19,41 @@ export const AdminSidebardata = [
   {
     title: "User",
     icon: <img src={user} />,
-    link:NAVIGATION_ROUTES.ADMIN_USERS,
+    link: NAVIGATION_ROUTES.ADMIN_USERS,
   },
   {
-    title:"Groups",
-    icon:<img src={groups}/>,
-    link:NAVIGATION_ROUTES.ADMIN_GROUPS
-  }
+    title: "Groups",
+    icon: <img src={groups} />,
+    link: NAVIGATION_ROUTES.ADMIN_GROUPS,
+  },
+  {
+    title: "Login",
+    icon: <img src={login} />,
+    link: NAVIGATION_ROUTES.LoggingIn,
+  },
+  {
+    title: "Billing",
+    icon: <img src={bill} />,
+    link: NAVIGATION_ROUTES.billing,
+  },
+  {
+    title: "Security",
+    icon: <img src={security} />,
+    link: NAVIGATION_ROUTES.security,
+  },
+  {
+    title: "Permission",
+    icon: <img src={permission} />,
+    link: NAVIGATION_ROUTES.permission,
+  },
+  {
+    title: "Manage Events",
+    icon: <img src={manageevents} />,
+    link: NAVIGATION_ROUTES.manageevents,
+  },
+  {
+    title: "Managed Workflow",
+    icon: <img src={workflow}/>,
+    link: NAVIGATION_ROUTES.manageworkflow,
+  },
 ];
--- src/Components/Sidebar/SideBarMenu/SideBarMenu.jsx
+++ src/Components/Sidebar/SideBarMenu/SideBarMenu.jsx
@@ -1,15 +1,27 @@
-import { Divider, Grid, Typography } from "@mui/material";
+import { Button, Divider, Grid, Typography } from "@mui/material";
 import React, { useState } from "react";
 import { SidebarData } from "../SidebarData";
 import { IoIosHelpCircleOutline, IoMdArrowDropdown } from "react-icons/io";
 import { useLocation, useNavigate } from "react-router-dom";
 import { AdminSidebardata } from "../AdminSidebardata";
+import HelpDropdown from "./HelpDropdown";
 
 export default function SideBarMenu({ isCollapsed, toggleCollapse }) {
   const [activeItem, setActiveItem] = useState(null);
   const navigate = useNavigate();
   const location = useLocation();
-  console.log(location.pathname.split("/")[1], ">>>>>>>>>>>>>>>>");
+
+ const [helpAnchorEl, setHelpAnchorEl] = useState(null);
+ const isHelpOpen = Boolean(helpAnchorEl);
+
+ const handleHelpClick = (event) => {
+   setHelpAnchorEl(event.currentTarget);
+ };
+
+ const handleHelpClose = () => {
+   setHelpAnchorEl(null);
+ };
+
 
   return (
     <Grid container spacing={2} direction="column">
@@ -29,7 +41,8 @@ export default function SideBarMenu({ isCollapsed, toggleCollapse }) {
                     alignItems: "center",
                     borderRadius: "8px",
                     height: "50px",
-                    gap: 1,
+                    gap: 2,
+                    my: 0.5,
                     padding: 1,
                     cursor: "pointer",
                     color:
@@ -51,21 +64,52 @@ export default function SideBarMenu({ isCollapsed, toggleCollapse }) {
                   {!isCollapsed && (
                     <Typography
                       sx={{ fontWeight: 600 }}
-                      fontSize="14px"
+                      fontSize="15px"
                       // color="rgb(10, 37, 64)"
                     >
                       {item.title}
                     </Typography>
                   )}
+
+                  {index === 0 && !isCollapsed && (
+                    <Button
+                      m={1}
+                      fontSize={10}
+                      sx={{
+                        marginLeft: "auto",
+                        textTransform: "none",
+                        fontWeight: "600",
+                        color: "#fff",
+                        backgroundColor: "#0B3558",
+                        height: "25px",
+                        padding: 0,
+                      }}
+                      fontWeight={600}
+                    >
+                      New
+                    </Button>
+                  )}
                 </Grid>
 
-                {index === 2 && (
-                  <Divider
-                    sx={{
-                      my: 1,
-                    }}
-                  />
-                )}
+                {index === 0 && !isCollapsed ? (
+                  <Typography
+                    m={1}
+                    fontSize={13}
+                    fontWeight={600}
+                    sx={{ padding: "8px 24px 8px 16px", color: "#476788" }}
+                  >
+                    Organization Settings
+                  </Typography>
+                ) : index === 5 ? (
+                  <Typography
+                    m={1}
+                    fontSize={13}
+                    fontWeight={600}
+                    sx={{ padding: "8px 24px 8px 16px", color: "#476788" }}
+                  >
+                    Scheduling Settings
+                  </Typography>
+                ) : null}
               </React.Fragment>
             ))}
           </>
@@ -84,8 +128,9 @@ export default function SideBarMenu({ isCollapsed, toggleCollapse }) {
                     alignItems: "center",
                     borderRadius: "8px",
                     height: "50px",
-                    gap: 1,
+                    gap: 2,
                     padding: 1,
+                    my: 0.5,
                     cursor: "pointer",
                     color:
                       location.pathname === item?.link ? "#0060e6" : "#111",
@@ -106,7 +151,7 @@ export default function SideBarMenu({ isCollapsed, toggleCollapse }) {
                   {!isCollapsed && (
                     <Typography
                       sx={{ fontWeight: 600 }}
-                      fontSize="14px"
+                      fontSize="15px"
                       // color="rgb(10, 37, 64)"
                     >
                       {item.title}
@@ -123,38 +168,44 @@ export default function SideBarMenu({ isCollapsed, toggleCollapse }) {
                 )}
               </React.Fragment>
             ))}
-            <Grid
-              item
-              sx={{
-                ml: 1,
-                p: 2,
-                display: "flex",
-                alignItems: "center",
-                borderRadius: "8px",
-                height: "50px",
-                gap: 1,
-                padding: 1,
-                cursor: "pointer",
-                border: "transparent",
-                backgroundColor: "transparent",
-                "&:hover": { backgroundColor: "#F2F8FF" },
-              }}
-            >
-              <IoIosHelpCircleOutline style={{ fontSize: "20px" }} />
-              {!isCollapsed && (
-                <Typography
-                  sx={{ fontWeight: 600 }}
-                  fontSize="14px"
-                  display={"flex"}
-                  alignContent={"center"}
-                  color="rgb(10, 37, 64)"
-                >
-                  Help <IoMdArrowDropdown fontSize="20px" />
-                </Typography>
-              )}
-            </Grid>
           </>
         )}
+        <Grid
+          onClick={handleHelpClick}
+          item
+          sx={{
+            ml: 1,
+            p: 2,
+            display: "flex",
+            alignItems: "center",
+            borderRadius: "8px",
+            height: "50px",
+            gap: 1,
+            padding: 1,
+            cursor: "pointer",
+            border: "transparent",
+            backgroundColor: "transparent",
+            "&:hover": { backgroundColor: "#F2F8FF" },
+          }}
+        >
+          <IoIosHelpCircleOutline style={{ fontSize: "20px" }} />
+          {!isCollapsed && (
+            <Typography
+              sx={{ fontWeight: 600 }}
+              fontSize="14px"
+              display={"flex"}
+              alignContent={"center"}
+              color="rgb(10, 37, 64)"
+            >
+              Help <IoMdArrowDropdown fontSize="20px" />
+            </Typography>
+          )}
+        </Grid>
+        <HelpDropdown
+          anchorEl={helpAnchorEl}
+          open={isHelpOpen}
+          onClose={handleHelpClose}
+        />
       </>
     </Grid>
   );
